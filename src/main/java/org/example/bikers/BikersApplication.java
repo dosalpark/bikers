967a497268a609635511ac903d4ad5c97f990870
pshistory <psh_a@naver.com>
2024-06-03 22:06:16 +0900

    init

---
+++ src/main/java/org/example/bikers/BikersApplication.java
@@ -0,0 +1,13 @@
+package org.example.bikers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bikers/BikersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bikers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
