417b2d8f2c2a677eafce84ffdcaf69acea2efb67
pshistory <psh_a@naver.com>
2024-06-08 18:00:18 +0900

    Feat : Jwt 토큰 공급자 생성
    토큰생성, 헤더토큰추출, 토큰검증 기능

---
+++ src/main/java/org/example/bikers/global/provider/JwtTokenProvider.java
@@ -0,0 +1,67 @@
+package org.example.bikers.global.provider;
+
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+    private final long TOKEN_TIME = 60 * 60 * 1000L;
+
+    @Value("${jwt.secret.token}")
+    private String secretToken;
+
+    private Key key;
+
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @PostConstruct
+    public void init(){
+        byte[] bytes = Base64.getDecoder().decode(secretToken);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String createAccessToken(Long userId, String email){
+        Date date = new Date();
+        return BEARER_PREFIX + Jwts.builder()
+            .claim("userId", userId)
+            .claim("email",email)
+            .setExpiration(new Date(date.getTime() + TOKEN_TIME))
+            .signWith(key, signatureAlgorithm)
+            .compact();
+    }
+
+    public String getJwtFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException |
+                 ExpiredJwtException e) {
+            return false;
+        }
+    }
+}
