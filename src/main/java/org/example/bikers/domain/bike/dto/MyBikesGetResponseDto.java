ca9083e08f7c569f4e51290759ff939bd8b8349e
pshistory <psh_a@naver.com>
2024-07-17 14:26:44 +0900

    Fix : MyBikes 전체조회 ResponseDto 수정
    기존 bikeModel관련된 값을 bikeModelId 만 가져오는 방식에서 bikeModelId로 join해서 manufacturer, bikeModelName, year, bikeCategory, displacement 를 가져오는방식으로 변경

--- src/main/java/org/example/bikers/domain/bike/dto/MyBikesGetResponseDto.java
+++ src/main/java/org/example/bikers/domain/bike/dto/MyBikesGetResponseDto.java
@@ -2,13 +2,19 @@ package org.example.bikers.domain.bike.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.example.bikers.domain.bikeModel.entity.BikeCategory;
+import org.example.bikers.domain.bikeModel.entity.Manufacturer;
 
 @Getter
 @Builder
 public class MyBikesGetResponseDto {
 
     private Long bikeId;
-    private Long bikeModelId;
+    private Manufacturer manufacturer;
+    private String bikeModelName;
+    private int year;
+    private BikeCategory bikeCategory;
+    private int displacement;
     private String nickName;
     private int mileage;
     private String bikeStatus;
