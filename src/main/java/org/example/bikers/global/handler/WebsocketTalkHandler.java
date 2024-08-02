825081fb0a52f1686d88700bb12dd2bb4a2f874a
pshistory <psh_a@naver.com>
2024-08-02 23:45:37 +0900

    Fix : ws 입장/퇴장/메세지전송 수정
    전송시 사용하는 sendMessage를 메소드로 분리하여서 입력한 사람이 ws 연결시 가져온 roomId에 해당하는 session에만 보내도록 설정

--- src/main/java/org/example/bikers/global/handler/WebsocketTalkHandler.java
+++ src/main/java/org/example/bikers/global/handler/WebsocketTalkHandler.java
@@ -1,8 +1,7 @@
 package org.example.bikers.global.handler;
 
-import java.util.HashMap;
+import java.io.IOException;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,36 +17,50 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class WebsocketTalkHandler extends TextWebSocketHandler {
 
     private final Set<WebSocketSession> sessionSet = new HashSet<>();
-    private final Map<Long, Set<WebSocketSession>> talkRoomMap = new HashMap<>();
 
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        //커넥션 연결
-        log.info("{} 접속", session.getId());
         sessionSet.add(session);
+        String getPayload = "님이 입장하셨습니다.";
+        sendMessage(session, getPayload, true);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status)
         throws Exception {
-        //커넥션 종료
-        log.info("{} 종료", session.getId());
+        String getPayload = "님이 나가셨습니다.";
+        sendMessage(session, getPayload, true);
+        sessionSet.remove(session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message)
         throws Exception {
-        //메세지 전송부
-
-        //1. 이용자가 보낸 메세지
         String getPayload = message.getPayload();
-        //2. 이용자 메세지를 보여줌
-        log.info("{}님: {}", session.getId(), getPayload);
-        //3. 서버가 이용자에게 보낼 메세지 생성
-        TextMessage msg = new TextMessage("server: hello");
-        //4. 이용자에게 발송
-        session.sendMessage(msg);
+        sendMessage(session, getPayload, false);
+    }
+
+    private void sendMessage(WebSocketSession session, String msg, boolean isSystemMessage) {
+        String email = session.getAttributes().get("email").toString();
+        String roomId = session.getAttributes().get("roomId").toString();
+        TextMessage textMessage;
+        if (isSystemMessage) {
+            textMessage = new TextMessage(email + msg);
+        } else {
+            textMessage = new TextMessage(email + " : " + msg);
+        }
+        sessionSet.parallelStream().forEach(otherSession -> {
+            try {
+                String otherSessionRoomId = otherSession.getAttributes().get("roomId").toString();
+                if (otherSession.isOpen()
+                    && roomId.equals(otherSessionRoomId)) {
+                    otherSession.sendMessage(textMessage);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
 }
